249f6dd4c5f3dbed97f30146d2056fb28c8c1aa5
Esuye302 <esrom1833@gmail.com>
2025-03-13 18:19:03 +0000

    deploy netlify

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 export const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5004/clone-9d32b/us-central1/api",
+  // baseURL: "http://127.0.0.1:5004/clone-9d32b/us-central1/api",
+  //deployed backend render
+  baseURL: "https://amazon-api-deploy-o804.onrender.com",
 });
 
